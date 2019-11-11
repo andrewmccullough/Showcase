ac1bcb503762890e43907e349e3f94accccf827c
Andrew McCullough <andrew.s.mccullough@gmail.com>
2019-11-11 14:22:58 -0500

    Code

---
+++ src/TestFile.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;       // for chrome
+
+public class TestFile {
+}
